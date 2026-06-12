f08d80b35a1097c055c5f77e741972cde0baf035
Rimon Bhowmik <rimonbhowmik2016@gmail.com>
2026-06-12 22:17:56 +0600

    Add files via upload

---
+++ Adition of two number.asm
@@ -0,0 +1,39 @@
+.MODEL SMALL
+.STACK 100H
+
+.DATA
+NUM1 DB 5
+NUM2 DB 3
+MSG DB 'Result of Addition: $'
+
+.CODE
+MAIN PROC
+
+    MOV AX, @DATA
+    MOV DS, AX
+
+    ; Display message
+    LEA DX, MSG
+    MOV AH, 09H
+    INT 21H
+
+    ; Load first number
+    MOV AL, NUM1
+
+    ; Add second number
+    ADD AL, NUM2
+
+    ; Convert result to ASCII
+    ADD AL, 30H
+
+    ; Display result
+    MOV DL, AL
+    MOV AH, 02H
+    INT 21H
+
+    ; Exit program
+    MOV AH, 4CH
+    INT 21H
+
+MAIN ENDP
+END MAIN
